1e24be697249b496fe2344f4876dd49ee4f5a816
Walter S L Junior <wjunino10@hotmail.com>
2023-05-22 00:57:50 -0300

    Fix: visual pág de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,17 @@
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo" >
+        <x-slot name="logo">
             <a href="/">
-               <!-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> -->
-               <div style="
+                <!-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> -->
+                <div style="
                             display: flex;
                             flex-direction: column;
                             flex-wrap: nowrap;
                             justify-content: center;
                             align-items: center;
-                            align-content: stretch;"
-                >
-                    <img src="../images/epicWinsdow.gif" alt="E-vortex logo"  style="width: 70px; padding-bottom:5px" class=""/>
-                    <img src="../images/evortex.png" alt="E-vortex logo"  style="width: 250px"/>
+                            align-content: stretch;">
+                    <!-- <img src="../images/shonnen.png" alt="E-vortex logo"  style="width: 70px; padding-bottom:5px" class=""/> -->
+                    <img src="../images/shonnen.png" alt="E-vortex logo" style="width: 700px" />
                 </div>
             </a>
         </x-slot>
@@ -21,47 +20,44 @@
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <x-auth-validation-errors class="mb-4 align-items: center;" :errors="$errors" />
+        <div class="container d-felx justofy-content-center">
+            <form class="row g-3 p-1 align-items: center;" method="POST" action="{{ route('login') }}">
+                @csrf
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                <!-- Email Address -->
+                <div>
+                    <x-input class="form-control" placeholder="Email" id="email" type="email" name="email" :value="old('email')" required autofocus />
+                </div>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                <!-- Password -->
+                <div>
+                    <x-input class="form-control" placeholder="Senha" id="password" type="password" name="password" required autocomplete="current-password" />
+                </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                <!-- Forgot password -->
+                <div class="flex items-center">
+                    <x-button class="ml-3 btn btn-danger">
+                        {{ __('Log in') }}
+                    </x-button>
+                    @if (Route::has('password.request'))
+                    <a class="text-sm btn link-danger" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
-                @endif
+                    @endif
+                </div>
+                <div>
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
+                </div>
+            </form>
+        </div>
     </x-auth-card>
 </x-guest-layout>
--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,21 +1,25 @@
 <div class="min-vh-100 container-xl py-5">
     <div class="card">
-        <div class="row">
-            <div class="col-4 p-3">
-            </div>
-            <div class="col-4 p-3 align-self-center">
-                {{ $logo }}
-            </div>
-            <div class="col-4 p-3">
+        <div class="card-header border p-1 m-1" style="background-color: #d22630;">
+            <div class="row m-1">
+                <div class="col-1 p-2">
+                </div>
+                <div class="col-10 p-2 align-self-center">
+                    {{ $logo }}
+                </div>
+                <div class="col-1 p-2">
+                </div>
             </div>
         </div>
-        <div class="card-body row">
-            <div class="col-4 p-5 m-1">
-            </div>
-            <div class="col-4 p-5 m-1">
-                {{ $slot }}
-            </div>
-            <div class="col-4 p-5 m-1">
+        <div class="card-footer border p-1 m-1 text-dark" style="background-color: #ffffff;">
+            <div class="row m-5">
+                <div class="col-4 m-2">
+                </div>
+                <div class="col-4 m-2">
+                    {{ $slot }}
+                </div>
+                <div class="col-4 m-2">
+                </div>
             </div>
         </div>
     </div>
